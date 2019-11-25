698e8a538f910efcd52e79808694dde8a9fe657b
Simon Felteau <simonfelteau@gmail.com>
2019-11-25 01:33:50 -0500

    tweaks and fixes

--- Rap_Export/rap_export.py
+++ Rap_Export/rap_export.py
@@ -6,6 +6,7 @@
 from LyricsGenius import lyricsgenius
 import os
 import json
+import copy
 
 genius = lyricsgenius.Genius("RnQTAFx2K7ec8RZu_ZTB9oCaUniy23M-HSXn8CryW8l1qqfRfuL6pAIj6E_SHhh6")
 genius.verbose = True
@@ -14,26 +15,30 @@ genius.skip_non_songs = True
 genius.excluded_terms = [("(Instrumental)", "(Remix)", "(Live)")]
 
 artists = ["Mos Def", "MF DOOM", "Aesop Rock", "Open Mike Eagle", "GZA"]
-lyricsFilter = ["[Skit", "[Bridge", "[Prod", "[Intro", "[Hook", "[Chorus", "[Interlude", "[Outro", "[Sample", "[Scratching", "[Verse", "[Part"]
-selectiveCount = 1
-completeCount = 3
+lyricsFilter = ["[Skit", "[Bridge", "[Prod", "[Intro", "[Hook", "[Chorus", "[Pre-Chorus", "[Interlude", "[Outro", "[Sample", "[Scratching", "[Verse", "[Part"]
+selectiveCount = 60
+completeCount = 120
 
 # Output Directory Creation
 if not os.path.exists("out"):
 	os.makedirs("out")
 os.chdir("out")
 
-def Filterer(data, filter, artist):
-	split1 = data.split(filter, 1)[0]
-	split2 = data.split(filter, 1)[1]
+def Filterer(dataF, filter, artist):
+	split1 = dataF.split(filter, 1)[0]
+	split2 = dataF.split(filter, 1)[1]
 	if filter == "[Verse" or filter == "[Part":
-		partEnd = split2.split("\n", 1)[0]
-		verse = split2.split("\n", 1)[1]
+		if split2.find("\n", 0) != -1:
+			partEnd = split2.split("\n", 1)[0]
+			verse = split2.split("\n", 1)[1]
+		else:
+			partEnd = split2
+			verse = split2
 		if partEnd.find(artist, 0) != -1 or partEnd.find(":", 0) == -1:
-			data = split1 + verse
-			if data.find(filter, 0, len(data)) != -1:
-				data = Filterer(data, filter, artist)
-			return data
+			dataF = split1 + verse
+			if dataF.find(filter, 0, len(dataF)) != -1:
+				dataF = Filterer(dataF, filter, artist)
+			return dataF
 		else:
 			partEndIndex = split2.find("\n\n", 0)
 			if partEndIndex != -1:
@@ -47,20 +52,19 @@ def Filterer(data, filter, artist):
 		else:
 			partEnd = split2
 	filteredTmp = split2.split(partEnd, 1)[1]
-	data = split1 + filteredTmp
+	dataF = split1 + filteredTmp
 
-	if data.find(filter, 0, len(data)) != -1:
-		data = Filterer(data, filter, artist)
-	return data
+	if dataF.find(filter, 0, len(dataF)) != -1:
+		dataF = Filterer(dataF, filter, artist)
+	return dataF
 
-def Untokenize(data):
-	split1 = data.split("\n", 1)[0]
-	split2 = data.split("\n", 1)[1]
-	data = split1 + "<endline>" + split2
-	print(data.find("\n", 0, len(data)))
-	if data.find("\n", 0, len(data)) != -1:
-		data = Untokenize(data)
-	return data
+def Untokenize(dataU):
+	split1 = dataU.split("\n", 1)[0]
+	split2 = dataU.split("\n", 1)[1]
+	dataU = split1 + "<endline>" + split2
+	if dataU.find("\n", 0, len(dataU)) != -1:
+		dataU = Untokenize(dataU)
+	return dataU
 
 
 for i in artists:
@@ -83,16 +87,19 @@ for i in artists:
 	for j in [f for f in os.listdir(".") if os.path.isfile(os.path.join(".", f))]:
 		if j.find("lyrics") != -1:
 			with open(j, "r") as currentSong:
+				print("Processing " + j)
 				tmp = json.load(currentSong)
 				for k in lyricsFilter:
 					filterIndex = tmp["songs"][0]["lyrics"].find(k, 0, len(tmp["songs"][0]["lyrics"]))
 					if filterIndex != -1: 
 						tmp["songs"][0]["lyrics"] = Filterer(tmp["songs"][0]["lyrics"], k, i)
 
-				untokenizedTmp = tmp
-				untokenizedTmp["songs"][0]["lyrics"] = Untokenize(untokenizedTmp["songs"][0]["lyrics"])
-
 				data["songs"].append(tmp["songs"][0])
+
+				untokenizedTmp = copy.deepcopy(tmp)
+				if untokenizedTmp["songs"][0]["lyrics"].find("\n", 0, len(untokenizedTmp["songs"][0]["lyrics"])) != -1:
+					untokenizedTmp["songs"][0]["lyrics"] = Untokenize(untokenizedTmp["songs"][0]["lyrics"])
+
 				untokenizedData["songs"].append(untokenizedTmp["songs"][0])
 
 	with open(i + "_Selective.json", "w+") as compiledSongs:
@@ -103,7 +110,7 @@ for i in artists:
 
 	# Artist Complete Seeking
 	os.chdir(i + "/Complete")
-	artistDisc = genius.search_artist(i, max_songs = completeCount, sort = "popularity", get_full_info = False)
+	artistDisc = genius.search_artist(i, sort = "popularity", get_full_info = False)
 	artistDisc.save_lyrics()
 
 	# Complete Result Compilation
@@ -112,17 +119,21 @@ for i in artists:
 	for j in [f for f in os.listdir(".") if os.path.isfile(os.path.join(".", f))]:
 		if j.find("lyrics") != -1:
 			with open(j, "r") as currentSong:
+				print("Processing " + j)
 				tmp = json.load(currentSong)
 				for k in lyricsFilter:
 					filterIndex = tmp["songs"][0]["lyrics"].find(k, 0, len(tmp["songs"][0]["lyrics"]))
 					if filterIndex != -1: 
 						tmp["songs"][0]["lyrics"] = Filterer(tmp["songs"][0]["lyrics"], k, i)
 
-				untokenizedTmp = tmp
-				untokenizedTmp["songs"][0]["lyrics"] = Untokenize(untokenizedTmp["songs"][0]["lyrics"])
-
 				data["songs"].append(tmp["songs"][0])
+
+				untokenizedTmp = copy.deepcopy(tmp)
+				if untokenizedTmp["songs"][0]["lyrics"].find("\n", 0, len(untokenizedTmp["songs"][0]["lyrics"])) != -1:
+					untokenizedTmp["songs"][0]["lyrics"] = Untokenize(untokenizedTmp["songs"][0]["lyrics"])
+
 				untokenizedData["songs"].append(untokenizedTmp["songs"][0])
+
 	with open(i + "_Complete.json", "w+") as compiledSongs:
 		json.dump(data, compiledSongs)
 	with open(i + "_Complete_Untokenized.json", "w+") as compiledSongs:
